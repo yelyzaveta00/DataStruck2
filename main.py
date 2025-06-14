7208dc41054b18cf36a1d0eb56190bc90f73513d
Liza <kholod.gmail.com>
2025-06-14 11:22:37 +0300

    нові зміни, додав фунцію для привітання  змінив основну функцію

--- main.py
+++ main.py
@@ -12,5 +12,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print("Hi!")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def Hello():
+    pass
